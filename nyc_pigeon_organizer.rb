baa83c79e76dc13e2f6e081e33f64c642293a856
Varvara Babkova <varvara.munday@gmail.com>
2019-06-11 20:25:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,8 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |property, property_hash|
+    property_hash.each do ||
+    end
+  end
+  return pigeon_list
 end
